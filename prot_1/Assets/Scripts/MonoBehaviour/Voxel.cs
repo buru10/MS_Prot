4a302c06060197ebf1a8b9af7eba1d341f5a2f94
morigoma <reo.y1105@gmail.com>
2019-10-29 14:14:10 +0900

    フォルダ、シーンの追加、別プロジェクトで作っていたものをマージ

---
+++ prot_1/Assets/Scripts/MonoBehaviour/Voxel.cs
@@ -0,0 +1,28 @@
+﻿//====================================================
+// Voxel
+// ボクセルの基本情報
+// author:Reo Yamakawa
+//====================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voxel : MonoBehaviour
+{
+    [SerializeField]
+    int ColorIndex;
+    Material ColorMat;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
