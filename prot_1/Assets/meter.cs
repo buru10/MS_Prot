d52037ec29f1bbe519f0534fabc4ac960bdf8d6b
kanaishida <kana.psvk.524@gmail.com>
2019-11-02 21:31:00 +0900

    プロト版メーターのUI追加

---
+++ prot_1/Assets/meter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class meter : MonoBehaviour
+{
+    public GameObject[] MemoryObj;
+
+    public GameObject ControllerUI;
+
+    public float MeterCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeterCount = 0;
+        for (int i = 0; i < MemoryObj.Length; i++)
+        {
+            MemoryObj[i].GetComponent<Image>().fillAmount = MeterCount;
+        }
+        ControllerUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            for (int i = 0; i < MemoryObj.Length; i++)
+            {
+                MeterCount += 0.02f;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            for (int i = 0; i < MemoryObj.Length; i++)
+            {
+                MeterCount -= 0.02f;
+            }
+        }
+
+        for (int i = 0; i < MemoryObj.Length; i++)
+        {
+            MemoryObj[i].GetComponent<Image>().fillAmount= MeterCount;
+        }
+
+        for (int i = 0; i < MemoryObj.Length; i++)
+        {
+            if(MemoryObj[i].GetComponent<Image>().fillAmount >= 1.0f)
+            {
+                ControllerUI.SetActive(true);
+            }
+        }
+    }
+}
+
+
+
