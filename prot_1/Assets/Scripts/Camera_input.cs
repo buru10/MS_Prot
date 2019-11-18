a990d4acc83a75e0acfcda4d18e4d4041969a7c0
kanaishida <kana.psvk.524@gmail.com>
2019-11-18 14:20:34 +0900

    カメラ操作実装

--- prot_1/Assets/Scripts/Camera_input.cs
+++ prot_1/Assets/Scripts/Camera_input.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class input : MonoBehaviour
+public class Camera_input : MonoBehaviour
 {
-
     public GameObject Player;
+    public GameObject PlayerHead;
+    public GameObject vCamera;
+
+    float rotatespeed = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -27,9 +30,14 @@ public class input : MonoBehaviour
         {
             Debug.Log("button2");
         }
+
+        Vector3 PlayerRotation = Player.transform.localEulerAngles;
+        //Yボタン
         if (Input.GetKeyDown("joystick button 3"))
         {
             Debug.Log("button3");
+            
+            PlayerHead.transform.rotation = Quaternion.Euler(0.0f, PlayerRotation.y, 0.0f);
         }
         if (Input.GetKeyDown("joystick button 4"))
         {
@@ -69,20 +77,22 @@ public class input : MonoBehaviour
         {
             Debug.Log("L stick:" + lsh + "," + lsv);
         }
-        if(lsh >= 0.1f)
+        if (lsh >= 0.1f)
         {
             Player.transform.Translate(0.1f, 0.0f, 0.0f);
-            
+            //PlayerHead.transform.Translate(0.1f, 0.0f, 0.0f);
+
         }
-        if (lsh <= -0.1f )
+        if (lsh <= -0.1f)
         {
             Player.transform.Translate(-0.1f, 0.0f, 0.0f);
-
+            //PlayerHead.transform.Translate(-0.1f, 0.0f, 0.0f);
 
         }
-        if ( lsv >= 0.1)
+        if (lsv >= 0.1)
         {
             Player.transform.Translate(0.0f, 0.0f, 0.1f);
+            //PlayerHead.transform.Translate(0.0f, 0.0f, 0.1f);
 
 
         }
@@ -90,8 +100,12 @@ public class input : MonoBehaviour
         {
 
             Player.transform.Translate(0.0f, 0.0f, -0.1f);
+            //PlayerHead.transform.Translate(0.0f, 0.0f, -0.1f);
 
         }
+
+        PlayerHead.transform.position = Player.transform.position;
+
         //R Stick
         float rsh = Input.GetAxis("R_Stick_H");
         float rsv = Input.GetAxis("R_Stick_V");
@@ -99,37 +113,72 @@ public class input : MonoBehaviour
         {
             Debug.Log("R stick:" + rsh + "," + rsv);
         }
+
+        //R Stick右
+        if (rsh >= 0.1f)
+        {
+            PlayerHead.transform.Rotate(0.0f, rotatespeed, 0.0f);
+
+
+        }
+        //R Stick左
+        if (rsh <= -0.1f)
+        {
+            PlayerHead.transform.Rotate(0.0f, -rotatespeed, 0.0f);
+
+
+        }
+
+
         //D-Pad
         float dph = Input.GetAxis("D_Pad_H");
         float dpv = Input.GetAxis("D_Pad_V");
-        float rotatespeed = 1.0f;
+        
+
+        // transformを取得
+        //Transform CameraTransform = Camera.transform;
+        // 座標を取得
+       // Vector3 Camerapos = CameraTransform.position;
+
         if ((dph != 0) || (dpv != 0))
         {
             Debug.Log("D Pad:" + dph + "," + dpv);
         }
+
+        //Dpad右
         if (dph == 1 && dpv == 0)
         {
             Player.transform.Rotate(0.0f, rotatespeed, 0.0f);
+            
 
         }
+        //Dpad左
         if (dph == -1 && dpv == 0)
         {
             Player.transform.Rotate(0.0f, -rotatespeed, 0.0f);
 
 
         }
+        //Dpad上
         if (dph == 0 && dpv == 1)
         {
             Player.transform.Translate(0.0f, 0.0f, 0.1f);
-
+            //PlayerHead.transform.Translate(0.0f, 0.0f, 0.1f);
+            //Camerapos.z += 0.1f;
 
         }
+        //Dpad下
         if (dph == 0 && dpv == -1)
         {
 
             Player.transform.Translate(0.0f, 0.0f, -0.1f);
+            //PlayerHead.transform.Translate(0.0f, 0.0f, -0.1f);
+            //Camerapos.z -= 0.1f;
 
         }
+
+        //CameraTransform.position = Camerapos;  // カメラ座標を設定
+
         //Trigger
         float tri = Input.GetAxis("L_R_Trigger");
         if (tri > 0)
@@ -142,7 +191,7 @@ public class input : MonoBehaviour
         }
         else
         {
-           // Debug.Log("  trigger:none");
+            // Debug.Log("  trigger:none");
         }
     }
 }
