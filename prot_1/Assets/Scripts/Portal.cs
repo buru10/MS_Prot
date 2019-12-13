6e98a31f83b8e032513dbeaf1cca06c7dba18dca
shun <shun8679@gmail.com>
2019-12-13 15:38:06 +0900

    ポータル　タイトル調整

---
+++ prot_1/Assets/Scripts/Portal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    // メンバ変数定義
+    private bool bWarp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bWarp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "PlayerCharactor")
+        {
+            bWarp = true;
+        }
+    }
+
+    public bool GetWarp()
+    {
+        return bWarp;
+    }
+}
