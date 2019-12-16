3be917770524e14ddc1f8b3c68d52e1c9e03a4f2
shun <shun8679@gmail.com>
2019-12-16 12:37:13 +0900

    タイトル画像プッシュ

---
+++ prot_1/Assets/Scripts/RisaikuruAI/RisaRamdomWarp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisaRamdomWarp : MonoBehaviour
+{
+    // メンバ変数定義
+    public float speed =1.0f;
+    float Savespeed;
+    public Vector2 X;
+    public Vector2 Z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Savespeed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed -= Time.deltaTime;
+
+        if(speed <= 0.0f)
+        {
+            speed = Savespeed;
+            gameObject.transform.position = new Vector3(Random.Range(X.x,X.y),0.0f, Random.Range(Z.x,Z.y));
+        }
+    }
+}
