6721ae7e6682edf826326344f38aadd7645642e8
shun <shun8679@gmail.com>
2019-12-19 00:11:46 +0900

    マップ追加　シネママシン調整

---
+++ prot_1/Assets/Scripts/Camera/DollyTrack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class DollyTrack : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera _virtualCamera;
+    [SerializeField]
+    private CinemachineDollyCart _DollyCart;
+
+    [SerializeField]
+    private CinemachineBrain _Brain;
+    [SerializeField]
+    private PlayerFollowCamera _FollowCamera;
+
+    private CinemachineTrackedDolly _dolly;
+
+    void Start()
+    {
+        // Virtual Cameraに対してGetCinemachineComponentでCinemachineTrackedDollyを取得する
+        // GetComponentではなくGetCinemachineComponentなので注意
+        _dolly = _virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
+    }
+
+    void Update()
+    {
+        // パスの位置を更新する
+        // 代入して良いのか不安になる変数名だけどこれでOK
+        _virtualCamera.transform.position = _DollyCart.transform.position;
+    }
+}
