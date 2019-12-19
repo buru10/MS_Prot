484e9608b2a1715fb0f670e29bd4543945924fa6
shun <shun8679@gmail.com>
2019-12-19 19:29:38 +0900

    エフェクト追加　

---
+++ prot_1/Assets/Scripts/Player/RisaSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisaSpawner : MonoBehaviour
+{
+    // メンバー変数定義
+    public List<GameObject> risaspawner = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for(int i=0; i<risaspawner;i++)
+        //Vector3 Apos = transform.position;
+        //Vector3 Bpos = risaspawner[i].transform.position;
+        //float dis = Vector3.Distance(Apos, Bpos);
+    }
+}
