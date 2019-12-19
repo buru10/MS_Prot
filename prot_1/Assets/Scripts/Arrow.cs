d19d68d6112eeea472393fb62b0cffe59f46cb9d
kanaishida <kana.psvk.524@gmail.com>
2019-12-19 12:43:54 +0900

    newMapCameraSceneを作成しました

---
+++ prot_1/Assets/Scripts/Arrow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    Vector3 scalemin;
+    Vector3 scalemax;
+    float timer;
+    float rotationSpeed;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        scalemin = new Vector3(0.137939f, 0.137939f, 0.137939f);
+        scalemax = new Vector3(0.2346939f, 0.2346939f, 0.2346939f);
+        rotationSpeed = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer>= 2.0f)
+        {
+            timer = 0.0f;
+        }
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        // ローカル座標を基準にした、サイズを取得
+        Vector3 localScale = myTransform.localScale;
+        if (timer <= 1.0f)
+        {
+            myTransform.localScale = Vector3.Lerp(scalemin, scalemax, timer);
+        }
+        else
+        {
+            myTransform.localScale = Vector3.Lerp(scalemax,scalemin,(timer%1.0f));
+        }
+        transform.Rotate(new Vector3(0, rotationSpeed, 0));
+    }
+}
---
+++ prot_1/Assets/Scripts/titlelogo.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class titlelogo : MonoBehaviour
+{
+    public GameObject plate_ShadeObj;
+    public GameObject plateObj;
+    public GameObject ringObj;
+    public GameObject[] logo_Obj=new GameObject[7];
+    public Sprite[] plate = new Sprite[2];
+    public Sprite[] plateshade = new Sprite[2];
+
+    int num;
+    float logotimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        num = 0;
+        logotimer = 0;
+        for (int i=0; i<logo_Obj.Length;i++)
+        {
+            logo_Obj[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        switch (num)
+        {
+            case 0:
+                logotimer += Time.deltaTime;
+                if(logotimer>=1.0f)
+                {
+                    logo_Obj[0].SetActive(true);
+                }
+                else if(logotimer >= 2.0f)
+                {
+                    logo_Obj[1].SetActive(true);
+                }
+
+                break;
+
+            default:
+                break;
+        }
+        ringObj.transform.Rotate(new Vector3(0, 0, 2.0f));
+
+    }
+}
