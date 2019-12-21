d9568f4a1f97034f2789e775aa94aa77e35ed545
kanaishida <kana.psvk.524@gmail.com>
2019-12-21 15:14:59 +0900

    skyboxのasset追加、変更しました。

--- prot_1/Assets/Scripts/titlelogo.cs
+++ prot_1/Assets/Scripts/titlelogo.cs
@@ -12,6 +12,13 @@ public class titlelogo : MonoBehaviour
     public Sprite[] plate = new Sprite[2];
     public Sprite[] plateshade = new Sprite[2];
 
+    Vector3 logopos_init;
+    Vector3 logopos_up;
+    Vector3 logopos_down;
+    Vector3 logopos_left;
+    Vector3 logopos_right;
+    public float movement;
+
     int num;
     float logotimer;
     // Start is called before the first frame update
@@ -19,6 +26,13 @@ public class titlelogo : MonoBehaviour
     {
         num = 0;
         logotimer = 0;
+        logopos_init = this.transform.position;
+        Debug.Log(logopos_init);
+       // logopos_up = new Vector3(logopos_init.x,logopos_init.y,logopos_init.z);
+        logopos_down = this.transform.position;
+        logopos_left = this.transform.position;
+        logopos_right = this.transform.position;
+
         for (int i=0; i<logo_Obj.Length;i++)
         {
             logo_Obj[i].SetActive(false);
