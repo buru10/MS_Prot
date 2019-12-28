11ccdf9df579efc3593cdab030bfb95a3e8f1493
kanaishida <kana.psvk.524@gmail.com>
2019-12-29 02:15:27 +0900

    マージ

--- prot_1/Assets/Scripts/titlelogo.cs
+++ prot_1/Assets/Scripts/titlelogo.cs
@@ -20,12 +20,14 @@ public class titlelogo : MonoBehaviour
     public float movement;
 
     int num;
-    float logotimer;
+    public float logotimer;
     float logointerval;
     int logoCount;
     public int movecount;
     Vector3[] logoscalmax;
     Vector3[] logoscalmin;
+
+    public float[] text_scale;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,12 +37,13 @@ public class titlelogo : MonoBehaviour
         logoCount = 0;
         movement = 20.0f;
 
-        logoscalmin[0] = logo_Obj[0].GetComponent<RectTransform>().localScale;
-        logoscalmax[0] = new Vector3(2.0f, 2.0f, 2.0f);
-        logo_Obj[0].GetComponent<RectTransform>().localScale= logoscalmax[0];
+        logoscalmax = new Vector3[logo_Obj.Length];
+        logoscalmin = new Vector3[logo_Obj.Length];
+        text_scale = new float[logo_Obj.Length];
+
 
         logopos_init = GetComponent<RectTransform>().localPosition;
-        //Debug.Log(logopos_init);
+
         logopos_up = new Vector3(logopos_init.x, logopos_init.y + movement, logopos_init.z);
         logopos_down = new Vector3(logopos_init.x, logopos_init.y - movement, logopos_init.z);
         logopos_left = new Vector3(logopos_init.x - movement, logopos_init.y, logopos_init.z);
@@ -48,9 +51,16 @@ public class titlelogo : MonoBehaviour
 
         for (int i = 0; i < logo_Obj.Length; i++)
         {
+            logoscalmax[i] = new Vector3(2.0f, 2.0f, 2.0f);
+            logoscalmin[i] = logo_Obj[i].GetComponent<RectTransform>().localScale;
+            logo_Obj[i].GetComponent<RectTransform>().localScale = logoscalmax[i];
+
             logo_Obj[i].SetActive(false);
+
         }
         ringObj.SetActive(false);
+
+
     }
 
     // Update is called once per frame
@@ -62,51 +72,54 @@ public class titlelogo : MonoBehaviour
         {
             case 0:
                 logotimer += Time.deltaTime;
-                if (logotimer >= logointerval * logoCount && logotimer <= logointerval * (logoCount + 0.5f))
+
+                //logotimer >=0.0f&&logotimer <=0.5//1.0&&1.5
+                if (logotimer >= logointerval * logoCount)
                 {
                     logo_Obj[logoCount].SetActive(true);
-                    
+                    logo_Obj[logoCount].GetComponent<RectTransform>().localScale = Vector3.Lerp(logoscalmax[logoCount], logoscalmin[logoCount], text_scale[logoCount]);
+                    text_scale[logoCount] += Time.deltaTime;
+
                 }
-                else if (logotimer >= logointerval * (logoCount + 1))
+
+                //logotimer >=1.0fになったら次の文字を出す
+                if (logotimer >= logointerval * (logoCount + 1))
                 {
                     logoCount += 1;
                 }
 
-                /*if (logotimer >= logointerval * logoCount && logotimer <= logointerval * (logoCount + 0.05f))
-                { if (movecount == 0)
-                    {
-                        GetComponent<RectTransform>().localPosition = logopos_up;
-                        movecount = 1;
-                    }
-                    else if (movecount == 1)
-                    {
-                        GetComponent<RectTransform>().localPosition = logopos_down;
-                        movecount = 2;
-                    }
-                    else if (movecount == 2)
-                    {
-                        GetComponent<RectTransform>().localPosition = logopos_left;
-                        movecount = 3;
-                    }
-                    else if (movecount == 3)
-                    {
-                        GetComponent<RectTransform>().localPosition = logopos_right;
-                        movecount = 0;
-                    }
-                    if (logoCount >= logo_Obj.Length)
-                    {
-                        ringObj.SetActive(true);
-                        num = 1;
-                    }
-                }*/
-                break;
-
-            /*case 1:
-                if ()
+                //logotimer >=0.0f&&logotimer <=0.05
+                /*  if (logotimer >= logointerval * (logoCount + 0.5f) && logotimer <= logointerval * (logoCount + 0.55f))
+                  {
+                      if (movecount == 0)
+                      {
+                          GetComponent<RectTransform>().localPosition = logopos_up;
+                          movecount = 1;
+                      }
+                      else if (movecount == 1)
+                      {
+                          GetComponent<RectTransform>().localPosition = logopos_down;
+                          movecount = 2;
+                      }
+                      else if (movecount == 2)
+                      {
+                          GetComponent<RectTransform>().localPosition = logopos_left;
+                          movecount = 3;
+                      }
+                      else if (movecount == 3)
+                      {
+                          GetComponent<RectTransform>().localPosition = logopos_right;
+                          movecount = 0;
+                      }
+                  }*/
+                //文字が全部出たらリングを出す
+                if (logoCount >= logo_Obj.Length)
                 {
-
+                    ringObj.SetActive(true);
+                    num = 1;
                 }
-                break;*/
+
+                break;
 
             default:
                 break;
