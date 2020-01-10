63b3fb943ceebf09fa28e83eb17f885200fff6aa
shun <shun8679@gmail.com>
2020-01-10 16:30:42 +0900

    リザルト、メインマップ二種調整、アニメーションの追加

---
+++ prot_1/Assets/Scripts/RisaikuruAI/RisaSpawnCount.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisaSpawnCount : MonoBehaviour
+{
+    // メンバ変数定義
+    private RisaikuruAIManager aIManager;
+    public GameObject risaObj;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!aIManager)
+            aIManager= GameObject.Find("RisaikuruAIManager").GetComponent<RisaikuruAIManager>();
+
+        for(int i =0;i<aIManager.GetRisaCount();i++)
+        {
+            Instantiate(risaObj, transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- prot_1/Assets/Scripts/RisaikuruAI/RisaikuruAIManager.cs
+++ prot_1/Assets/Scripts/RisaikuruAI/RisaikuruAIManager.cs
@@ -5,13 +5,8 @@ using UnityEngine;
 public class RisaikuruAIManager : MonoBehaviour
 {
     // メンバ変数定義
-    public Player Player;
-    public Vector3 PlayerMarker;
-
     private int CreateNumber;
     private int nCnt;
-    private int nNum;
-    private int nNumSave;
 
     public static int CountRisa;
 
@@ -22,47 +17,16 @@ public class RisaikuruAIManager : MonoBehaviour
         DontDestroyOnLoad(this);
 
         // 変数初期化
-        nCnt = 0;
-        CreateNumber = 1;
         CountRisa = 0;
-
-        if (!Player)
-            Player = GameObject.Find("Player").GetComponent<Player>();
-       
-        // 子供の情報を受け取る
-        foreach (Transform child in Player.transform)
-        {
-            if ("Back" == child.name)
-                PlayerMarker = child.transform.position;
-        }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       // Debug.Log(CountRisa);
-    }
-
-    public int GetCreateNumber()
-    {
-        return CreateNumber;
-    }
-    public void SetCreateNumberPlus()
-    {
-        CreateNumber++;
-    }
-
-    public Vector3 GetPlayerMarker(int CreateNum)
-    {
-        Vector3 transform = PlayerMarker;
-        nNum = CreateNum;
-
-        return transform;
     }
 
     public void AddRisa()
     {
         CountRisa++;
+    }
+
+    public int GetRisaCount()
+    {
+        return CountRisa;
     }
 }
