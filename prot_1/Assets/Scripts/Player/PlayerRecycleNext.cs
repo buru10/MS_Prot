553210c1ac9fdeac0c21f5f2c003bdb6d7d0e8bb
shun <shun8679@gmail.com>
2020-01-17 10:36:05 +0900

    リザルト文字調整、マップの色々調整

---
+++ prot_1/Assets/Scripts/Player/PlayerRecycleNext.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRecycleNext : MonoBehaviour
+{
+    public GarbageManager2 manager;
+    private UImove imove;
+    [SerializeField] SceneChanger sceneChanger;
+    [SerializeField] StageStateManager ssm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        imove = GetComponent<UImove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (manager.GetPercentage() >= 100)
+        {
+            imove.enabled = true;
+            if (imove.GetTrigger2())
+            {
+                sceneChanger.ChangeToNext();
+                ssm.ChangeState(StageStateManager.StageState.ShutDown);
+            }
+        }
+    }
+}
